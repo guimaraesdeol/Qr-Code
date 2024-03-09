9be604cce0beaa9fcdab1017d6d84f6f4e6ef9ee
Eduardo Guimarães <139907650+guimaraesdeol@users.noreply.github.com>
2024-03-09 00:34:35 -0300

    Info

--- qrcodepython.py
+++ qrcodepython.py
@@ -3,7 +3,7 @@ qr = qrcode.QRCode(version=1,
                    error_correction=qrcode.constants.ERROR_CORRECT_L,
                    box_size=20,
                    border=2)
-qr.add_data("https://www.youtube.com/watch?v=xvFZjo5PgG0")
+qr.add_data("https://www.youtube.com/watch?v=xvFZjo5PgG0") # INSIRA AQUI SEU LINK OU INFORMAÇÃO
 qr.make(fit=True)
 
 img = qr.make_image(fill_color="black", back_color="white")
